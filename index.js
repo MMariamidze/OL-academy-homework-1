60fdb0f1e197114245a7a5e91fed0298776d6a28
MMariamidze <104030318+MMariamidze@users.noreply.github.com>
2022-09-17 10:42:56 +0400

    up

---
+++ index.js
@@ -0,0 +1,15 @@
+function creatTable(){
+	
+ let newTable = document.getElementById("table")
+ let tr = document.createElement('tr');
+ tr.classList.add('myTr')
+ let td = document.createElement('td');
+ tr.appendChild(td);
+ newTable.appendChild(tr);
+	
+}
+
+
+
+
+
